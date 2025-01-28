f39767ae27e0b923c16fe2b377575987c5181c55
MonseLopez <22300007@uttt.edu.mx>
2025-01-27 19:08:48 -0600

    Ccambios realizados

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,7 @@
 document.getElementById("advancedForm").addEventListener("submit", function (e) {
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
     const age = document.getElementById("age").value;
-    const password = document.getElementById("password").value;
 
-    if (!name || !email || !age || !password) {
-        e.preventDefault();
-        alert("Todos los campos son obligatorios.");
-    } else if (age < 18 || age > 100) {
+    if (age < 18 || age > 100) {
         e.preventDefault();
         alert("Por favor, ingresa una edad válida (18-100 años).");
     }
